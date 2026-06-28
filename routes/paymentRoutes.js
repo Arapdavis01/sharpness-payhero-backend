ec01d19bbe28c72fd23118dc90c8e8575813db35
Arapdavis01 <dancun6742@gmail.com>
2026-06-29 00:11:06 +0300

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,8 @@
-// routes/paymentRoutes.js
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
+// Both controller functions must exist and be exported
 router.post('/initiate', paymentController.initiatePayment);
 router.post('/callback', paymentController.handleCallback);
 
